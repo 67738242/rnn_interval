107956fca376eecb23e9d2b793b0f44387949eda
67738242 <masaki@c.info.eng.osaka-cu.ac.jp>
2019-10-25 17:47:01 +0900

    learning_rate = 0.01 # when attention,learning_rate must be 0.001 learning_data_day_len = 20 input_digits = 100 output_digits = 20 n_hidden = 40 epochs = 1000 eary_stop_epoch = 150 batch_size = 30 attention_layer_size = 10 num_units = 50 ample = 0 # day = 'Tue' # learning_length = 700 thrd = 54.5 tchr_frcng_thr = 0.8

--- rnn_intrvl.py
+++ rnn_intrvl.py
@@ -30,8 +30,8 @@ from sklearn.model_selection import train_test_split
 
 learning_rate = 0.01
 # when attention,learning_rate must be 0.001
-learning_data_day_len = 200
-input_digits = 1000
+learning_data_day_len = 20
+input_digits = 100
 output_digits = 20
 n_hidden = 40
 epochs = 1000
