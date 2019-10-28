57e1340c5f77a248a03648e48662f7ffe3ecd4f5
正木　健太 <masaki@masakiktanoiMac.Z3615>
2019-10-28 16:42:12 +0900

    trest

--- rnn_intrvl.py
+++ rnn_intrvl.py
@@ -427,7 +427,7 @@ early_stopping = Early_Stopping(patience=10, verbose=1)
 for epoch in range(epochs):
     X_, Y_ = shuffle(input_data_train, true_data_train)
     with tf.name_scope('train'):
-        for h in range(100):
+        for h in range(n_batches):
             start = h * batch_size
             end = start + batch_size
             print(np.shape(X_[start:end]))
